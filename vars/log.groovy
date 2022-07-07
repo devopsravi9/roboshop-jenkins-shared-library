8db79c4eab5a323c97452a454afd8ef25e95187f
Ravi <ravi@gmail.com>
2022-07-07 12:08:47 +0530

    ec2 code

--- vars/log.groovy
+++ vars/log.groovy
@@ -6,20 +6,38 @@ def warning(message) {
     echo "WARNING: ${message}"
 }
 
-def call(InDirectVar) {
-    pipeline {
-        agent any
-        stages {
-            stage('one') {
-                steps {
-                    sh "echo one- ${InDirectVar}"
-                }
-            }
-            stage('two') {
-                steps {
-                    sh 'echo two- ${DirectVar}'
-                }
-            }
+// this is declarative approch. but we are going scripted approach for shared library. lets see
+
+//
+//def call(InDirectVar) {
+//    pipeline {
+//        agent any
+//        stages {
+//            stage('one') {
+//                steps {
+//                    sh "echo one- ${InDirectVar}"
+//                }
+//            }
+//            stage('two') {
+//                steps {
+//                    sh 'echo two- ${DirectVar}'
+//                }
+//            }
+//        }
+//    }
+//}
+
+// SCRIPTED approach. IN scripted approach we can also write code in between them.
+
+def call () {
+    node () {
+        stage ('one') {
+            sh "echo one"
+        }
+
+
+        stage('two') {
+            sh "two - ${DirectVar}"
         }
     }
 }
