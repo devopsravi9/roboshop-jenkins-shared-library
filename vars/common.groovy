7756531ed686f175c4df7dc7c70de1ffff605547
Ravi <ravi@gmail.com>
2022-07-07 14:06:41 +0530

    ec2 code

--- vars/common.groovy
+++ vars/common.groovy
@@ -3,7 +3,7 @@
 def pipelineInit() {
         stage('clear old files & cloneing git again') {
             sh 'rm -rf *'
-            git branch: 'main', url: 'https://github.com/devopsravi9/cart.git'
+            git branch: 'main', url: "https://github.com/devopsravi9/${COMPONENT}.git"
 
         }
     }
