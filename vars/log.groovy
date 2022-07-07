3df8013b89cb0f568d617b2fb5188014c5999f66
Ravi <ravi@gmail.com>
2022-07-07 11:22:38 +0530

    ec2 code

--- vars/log.groovy
+++ vars/log.groovy
@@ -5,3 +5,23 @@ def info(message) {
 def warning(message) {
     echo "WARNING: ${message}"
 }
+
+def hello() {
+    pipeline {
+        agent any
+        satges {
+            stage('one') {
+                steps {
+                    sh 'echo one'
+                }
+            }
+        }
+        satges {
+            stage('two') {
+                steps {
+                    sh 'echo two'
+                }
+            }
+        }
+    }
+}
