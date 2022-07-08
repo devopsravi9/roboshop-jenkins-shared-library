2465e91535ee52cb8b2a52d5b7ba4744a3e7b165
Ravi <ravi@gmail.com>
2022-07-08 11:55:19 +0530

    ec2 code

---
+++ vars/python.groovy
@@ -0,0 +1,14 @@
+def cal() {
+    node () {
+
+        common.pipelineInit()
+
+        if ( env.BRANCH_NAME == env.TAG_NAME ) {
+            common.PublishArtiFacts()
+        }
+    }
+}
+
+// in python script we download dependencies on server side not on ci side
+//so we zip all the files. in those files requirements.txt contains the list of dependencies to be
+//downloaded on server side.
