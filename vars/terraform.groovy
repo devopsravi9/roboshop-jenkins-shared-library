d5d79a252f415fb2505459f964f90ba29b773f75
Ravi <ravi@gmail.com>
2022-07-28 12:00:15 +0530

    ec2 code

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,16 +1,21 @@
 def call() {
     node {
+        properties([
+            parameters([
+                choice(choices: ['dev', 'prod'], description: "choose environment", name: "ENV"),
+                ])
+        ])
         ansiColor('xterm') {
             stage('terraform init') {
-                sh 'terraform init'
+                sh 'terraform init -backend-config=env/S{ENV}-backend.tfvars'
             }
 
             stage ('terraform plan') {
-                sh 'terraform plan'
+                sh 'terraform plan -var-file=env/${ENV}.tfvars'
             }
 
             stage ('terraform apply') {
-                sh 'terraform apply -auto-approve'
+                sh 'terraform apply -auto-approve -var-file=env/${ENV}.tfvars'
             }
         }
     }
