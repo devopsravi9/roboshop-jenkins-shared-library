3cd96a3a745fdc159deff56ebe47eac35766d65d
Ravi <ravi@gmail.com>
2022-07-28 14:27:03 +0530

    ec2 code

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -8,7 +8,7 @@ def call() {
         ansiColor('xterm') {
             stage ('Code Checkout') {
                 sh 'rm -rf *'
-                sh 'find . | sed -e '1d' | xargs rm -rf  '
+                sh 'find .  '
                 git branch: 'main', url: 'https://github.com/devopsravi9/roboshop-terraform-mutable.git'
             }
 
