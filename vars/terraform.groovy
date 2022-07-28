a18543b5be2835f55c5861c5843dce2847c5dc23
Ravi <ravi@gmail.com>
2022-07-28 10:38:13 +0530

    ec2 code

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,17 +1,17 @@
 def call() {
     node {
         ansiColor('xterm') {
-        stage('terraform init') {
-            sh 'terraform init'
-        }
+            stage('terraform init') {
+                sh 'terraform init'
+            }
 
-        stage ('terraform plan') {
-            sh 'terraform plan'
-        }
+            stage ('terraform plan') {
+                sh 'terraform plan'
+            }
 
-        stage ('terraform apply') {
-            sh 'terraform apply -auto-approve'
-        }
+            stage ('terraform apply') {
+                sh 'terraform apply -auto-approve'
+            }
         }
     }
 }
